30fef7c56627653ca383c12f838798934d266cf4
shreya <shreyajdoddamani@gmail.com>
2025-04-14 14:21:43 +0530

    port

--- api/index.js
+++ api/index.js
@@ -257,7 +257,7 @@ app.delete("/tickets/:id", async (req, res) => {
    }
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`);
 });
